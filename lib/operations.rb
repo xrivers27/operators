509ab84a06449f60055ccdd099011e0a00b21b9a
xrivers27 <xrivers27@gmail.com>
2020-10-05 12:47:48 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,35 @@
+require 'pry'
+
+
 def unsafe?(speed)
+ if speed < (40)
+    return true
+ elsif speed > (60)
+    return true
+ else 
+    return false
+ end
 
 end
 
+
+unsafe?(35)
 
 
 def not_safe?(speed)
-	
+	speed <40 || speed >60 ? true : false
 end
-	
 
+not_safe?(35)
+
+# Variable
+# speed = 35
+# speed
+
+# Method
+# def cow_tipping(x)
+# puts x
+# end
+
+# cow_tipping("gdksldsdkasjdkajh")
 
